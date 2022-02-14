6fd5ef09e5ea2f51cc5e815a3e0ad3f22ecdcf83
hygenie1228 <namhj28@gmail.com>
2022-02-14 06:08:29 +0000

    change head conv

--- lib/models/model.py
+++ lib/models/model.py
@@ -39,6 +39,7 @@ class Model(nn.Module):
 
     def forward_contrastive(self, inp_img, meta_hm):
         img_feat = self.backbone(inp_img)
+        img_feat = self.head(img_feat)
 
         # hm normalization
         meta_hm = meta_hm.clone()
@@ -51,11 +52,8 @@ class Model(nn.Module):
         batch_size, joint_num, _ = joint_feat.shape
         joint_feat = joint_feat.view(batch_size*joint_num, -1)
 
-        joint_feat = self.head(joint_feat)
-
         joint_feat = F.normalize(joint_feat, dim=1)
         joint_feat = joint_feat.view(batch_size, joint_num, -1)
-
         return joint_feat
 
     def forward_2d_joint(self, inp_img):
@@ -164,7 +162,7 @@ def get_model(is_train):
         
 
     if cfg.MODEL.type == 'contrastive':
-        head = Projector(backbone_out_dim,cfg.MODEL.projector_hidden_dim,cfg.MODEL.projector_out_dim)
+        head = nn.Conv2d(in_channels=backbone_out_dim, out_channels=cfg.MODEL.projector_out_dim, kernel_size=1, stride=1,padding=0)
     elif cfg.MODEL.type == '2d_joint':
         head = nn.Conv2d(in_channels=backbone_out_dim, out_channels=coco.joint_num, kernel_size=1, stride=1,padding=0)
     elif cfg.MODEL.type == 'body':
