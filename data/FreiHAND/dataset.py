ee63dc2a6c964b14f24ae780bcb8c4c2ded24779
TaeryungLee <trlee94@snu.ac.kr>
2022-02-18 21:17:51 +0900

    hand

--- data/FreiHAND/dataset.py
+++ data/FreiHAND/dataset.py
@@ -1,34 +1,29 @@
-
 import os
 import os.path as osp
 import numpy as np
 import torch
 import cv2
-import random
 import json
-import math
 import copy
-import scipy.sparse
-import transforms3d
-
 from pycocotools.coco import COCO
-from core.config import cfg 
-from coord_utils import process_bbox, get_bbox
+from core.config import cfg
+from human_models import mano
+from coord_utils import get_bbox, process_bbox
+# from preprocessing import load_img, process_bbox, augmentation, process_human_model_output, get_bbox
 
-from _mano import MANO
-from graph_utils import build_coarse_graphs
-from aug_utils import j2d_processing
-
+from vis_utils import vis_keypoints, save_obj
+from base_dataset import BaseDataset
 
 
 class FreiHAND(torch.utils.data.Dataset):
-    def __init__(self, mode, args):
+    def __init__(self, transform, data_split):
         dataset_name = 'FreiHAND'
-        self.data_split = mode
-        self.data_path = osp.join(cfg.data_dir, dataset_name, 'data')
+        self.data_split = data_split
+        self.transform = transform
+        self.data_path = osp.join('data', 'FreiHAND', 'data')
 
         # MANO joint set
-        self.mesh_model = MANO()
+        # self.mesh_model = MANO()
         self.face = self.mesh_model.face
         self.joint_regressor_mano = self.mesh_model.joint_regressor
         self.vertex_num = self.mesh_model.vertex_num
@@ -43,9 +38,6 @@ class FreiHAND(torch.utils.data.Dataset):
         det_path = osp.join(self.data_path, f'hrnet_output_on_{mode}set.json')
         self.datalist_pose2d_det = self.load_pose2d_det(det_path)
         print("Check lengths of annotation and detection output: ", len(self.datalist), len(self.datalist_pose2d_det))
-
-        self.graph_Adj, self.graph_L, self.graph_perm, self.graph_perm_reverse = \
-            build_coarse_graphs(self.mesh_model.face, self.joint_num, self.skeleton, self.joint_hori_conn, levels=6)
 
     def load_pose2d_det(self, det_path):
         datalist = []
@@ -106,86 +98,5 @@ class FreiHAND(torch.utils.data.Dataset):
 
         return sorted(datalist, key=lambda d: d['img_id'])
 
-    def get_mano_coord(self, mano_param, cam_param):
-        pose, shape = mano_param['pose'], mano_param['shape']
-        # mano parameters (pose: 48 dimension, shape: 10 dimension)
-        mano_pose = torch.FloatTensor(pose).view(-1, 3)
-        mano_shape = torch.FloatTensor(shape).view(1, -1)
-        # camera rotation and translation
-        R, t = np.array(cam_param['R'], dtype=np.float32).reshape(3, 3), np.array(cam_param['t'], dtype=np.float32).reshape(3)
-        mano_trans = torch.from_numpy(t).view(-1, 3)
-
-        # transform world coordinate to camera coordinate
-        root_pose = mano_pose[self.root_joint_idx, :].numpy()
-        angle = np.linalg.norm(root_pose)
-        root_pose = transforms3d.axangles.axangle2mat(root_pose / angle, angle)
-        root_pose = np.dot(R, root_pose)
-        axis, angle = transforms3d.axangles.mat2axangle(root_pose)
-        root_pose = axis * angle
-        mano_pose[self.root_joint_idx] = torch.from_numpy(root_pose)
-        mano_pose = mano_pose.view(1, -1)
-
-        # get mesh and joint coordinates
-        mano_mesh_coord, mano_joint_coord = self.mesh_model.layer(mano_pose, mano_shape, mano_trans)
-        mano_mesh_coord = mano_mesh_coord.numpy().reshape(self.vertex_num, 3);
-        mano_joint_coord = mano_joint_coord.numpy().reshape(self.joint_num, 3)
-
-        return mano_mesh_coord, mano_joint_coord
-
     def __len__(self):
         return len(self.datalist)
-
-    def __getitem__(self, idx):
-        data = copy.deepcopy(self.datalist[idx])
-        img_id, img_path, img_shape, joint_cam, cam_param, mano_param = \
-            data['img_id'], data['img_path'], data['img_shape'],  data['joint_cam'], data['cam_param'], data['mano_param']
-        rot, flip = 0, 0
-
-        # mano coordinates
-        mano_mesh_cam, mano_joint_cam = self.get_mano_coord(mano_param, cam_param)
-        mano_coord_cam = np.concatenate((mano_mesh_cam, mano_joint_cam))
-        # cam -> image projection
-        # focal, princpt = cam_param['focal'], cam_param['princpt']
-        # joint_coord_img = cam2pixel(mano_joint_cam, focal, princpt)[:, :2]
-
-        # root align cam mesh/joint
-        mano_coord_cam = mano_coord_cam - mano_joint_cam[:1]
-        mesh_coord_cam = mano_coord_cam[:self.vertex_num];
-        joint_coord_cam = mano_coord_cam[self.vertex_num:];
-
-        # use det
-        det_data = self.datalist_pose2d_det[idx]
-        assert img_id == det_data['img_id']
-        joint_coord_img = det_data['img_joint']
-
-        # make bbox
-        bbox = get_bbox(joint_coord_img)
-        bbox = process_bbox(bbox.copy())
-
-        # aug
-        joint_coord_img, trans = j2d_processing(joint_coord_img.copy(),
-                                                (cfg.MODEL.input_shape[1], cfg.MODEL.input_shape[0]),
-                                                bbox, rot, flip, None)
-        #  -> 0~1
-        joint_coord_img = joint_coord_img[:, :2]
-        joint_coord_img /= np.array([[cfg.MODEL.input_shape[1], cfg.MODEL.input_shape[0]]])
-
-        # normalize loc&scale
-        mean, std = np.mean(joint_coord_img, axis=0), np.std(joint_coord_img, axis=0)
-        joint_coord_img = (joint_coord_img.copy() - mean) / std
-
-        if cfg.MODEL.name == 'pose2mesh_net':
-            # default valid
-            mesh_valid = np.ones((len(mesh_coord_cam), 1), dtype=np.float32)
-            reg_joint_valid = np.ones((len(joint_coord_cam), 1), dtype=np.float32)
-            lift_joint_valid = np.ones((len(joint_coord_cam), 1), dtype=np.float32)
-
-            inputs = {'pose2d': joint_coord_img}
-            targets = {'mesh': mesh_coord_cam / 1000, 'lift_pose3d': joint_coord_cam, 'reg_pose3d': joint_coord_cam}
-            meta = {'mesh_valid': mesh_valid, 'lift_pose3d_valid': lift_joint_valid, 'reg_pose3d_valid': reg_joint_valid}
-            return inputs, targets, meta
-
-        elif cfg.MODEL.name == 'posenet':
-            # default valid
-            joint_valid = np.ones((len(joint_coord_cam), 1), dtype=np.float32)
-            return joint_coord_img, joint_coord_cam, joint_valid
